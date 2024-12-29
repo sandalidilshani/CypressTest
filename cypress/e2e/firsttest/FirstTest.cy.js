e2f1d8729e978ad52b92dd0d45d7772725c20697
sandalidilshani <rkpsandalidilshani@gmail.com>
2024-12-29 18:21:52 +0530

    user detail

--- cypress/e2e/firsttest/FirstTest.cy.js
+++ cypress/e2e/firsttest/FirstTest.cy.js
@@ -15,7 +15,20 @@ describe('Buy the bag', () => {
     cy.get('button[id="add-to-cart-sauce-labs-backpack"]', { timeout: 10000 }).click();
     cy.get('.shopping_cart_link').click();
     cy.url().should('include', '/cart.html'); 
-    cy.get('button').contains('Checkout').click();
+    cy.get('button[id="checkout"]', { timeout: 10000 }).click();
+  }); 
+
+  it('allow users to add their details to pay', () => {
+    cy.url().should('include', '/checkout-step-one.html'); 
+    cy.get('.title').should('contain', 'Checkout');
     
-});
+    // Add form filling actions
+    cy.get('#first-name').type('John');
+    cy.get('#last-name').type('Doe');
+    cy.get('#postal-code').type('12345');
+    cy.get('#continue').click();
+    
+    // Optional: Add assertion for successful form submission
+    cy.url().should('include', '/checkout-step-two.html');
+  });
 });
